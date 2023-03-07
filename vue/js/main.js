58c5d7a537ace6bd6e74ac0e90fd527fda455146
Snezhana <cnega.2002@gmail.com>
2023-03-07 14:17:09 +0700

    8 PR

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,11 +1,45 @@
-let app = new Vue({
-    el: '#app',
-    data: {
+
+
+
+
+ Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    }, 
+    
+    template: `
+ <div class="product">
+    <div class="product-image">
+        <img :src="image" :alt="altText"/>
+    </div>
+    <div class="product-info">
+        <h1>{{ title}}</h1>
+        <p v-if="inStock">In stock</p>
+        <p v-else class="textDecoration ">Out of Stock</p>
+        <p>{{ sale}}</p>
+        <product-details></product-details>
+        <p>Shipping: {{ shipping }}</p>
+        <div class="color-box" v-for="(variant, index) in variants" :key="variant.variantId" :style="{ backgroundColor:variant.variantColor }" 
+        @mouseover="updateProduct(index)">
+        </div>
+        <div class="cart">
+            <p>Cart({{ cart }})</p>
+        </div>
+        <button @click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to cart</button>
+        <button v-if="cart > 0" @click="deleteToCart">Delete to cart</button>
+        <button v-else class="disabledButton">Delete to cart</button>
+    </div>
+ </div>
+`,
+data() {
+    return {
         product: "Socks",
         brand: 'Vue Mastery',
         selectedVariant: 0,
         altText: "A pair of socks",
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
@@ -22,40 +56,64 @@ let app = new Vue({
         ],
         cart: 0,
         onSale: true
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateProduct(index) {
-            this.selectedVariant = index;
-            console.log(index);
-         },
-        deleteToCart(){
-            this.cart = this.cart - 1
-        } 
-     },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-         },
-        inStock(){
-            return this.variants[this.selectedVariant].variantQuantity
-        },
-        sale(){
-            if (this.onSale){
-                return this.brand + ' ' + this.product + ' ' + " on sale";
-            }
-            
-        }
-         
-         
-        
     }
+},
+methods: {
+    addToCart() {
+        this.cart += 1
+    },
+    updateProduct(index) {
+        this.selectedVariant = index;
+        console.log(index);
+     },
+    deleteToCart(){
+        this.cart = this.cart - 1
+    } 
+},
+computed: {
+    title() {
+        return this.brand + ' ' + this.product;
+    },
+    image() {
+        return this.variants[this.selectedVariant].variantImage;
+     },
+    inStock(){
+        return this.variants[this.selectedVariant].variantQuantity
+    },
+    sale(){
+        if (this.onSale){
+            return this.brand + ' ' + this.product + ' ' + " on sale";
+        }
+        
+    },
+    shipping() {
+        if (this.premium) {
+            return "Free";
+        } else {
+            return 2.99
+        }
+     }
      
- })
+}
+})
+
+Vue.component('product-details',{
+    template: `
+    <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+ `,
+ data(){
+    return{
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    }
+ }
+})
+
  
- 
+let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true
+    }
+})
