5fc9af384fbeeec8a69b92d308539087ca6e712a
Snezhana <cnega.2002@gmail.com>
2023-03-07 15:43:23 +0700

    10 PR

--- vue/js/main.js
+++ vue/js/main.js
@@ -7,7 +7,7 @@ Vue.component('product', {
     },
     template: `
     <div class="product">
-     <div class="product-image">
+        <div class="product-image">
             <img :src="image" :alt="altText"/>
         </div>
  
@@ -36,6 +36,20 @@ Vue.component('product', {
             <button v-on:click="deleteToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Delete to cart
             </button>
         </div>
+        <div>
+            <h2>Reviews</h2>
+            <p v-if="!reviews.length">There are no reviews yet.</p>
+            <ul>
+                <li v-for="review in reviews">
+                    <p>{{ review.name }}</p>
+                    <p>Rating: {{ review.rating }}</p>
+                    <p>{{ review.review }}</p>
+                </li>
+            </ul>
+        </div>
+        
+
+        <product-review @review-submitted="addReview"></product-review>
     </div>
   `,
     data() {
@@ -59,6 +73,7 @@ Vue.component('product', {
                     variantQuantity: 0
                 }
             ],
+            reviews: [],
         }
     },
     methods: {
@@ -71,6 +86,9 @@ Vue.component('product', {
         },
         deleteToCart(){
             this.$emit('delete-to-cart', this.variants[this.selectedVariant].variantId);
+        },
+        addReview(productReview){
+            this.reviews.push(productReview)
         }
     },
     computed: {
@@ -92,11 +110,91 @@ Vue.component('product', {
         }
     }
  })
+
+ Vue.component('product-review', {
+    template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+    <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+      <li v-for="error in errors">{{ error }}</li>
+    </ul>
+   </p>
+ <p>
+   <label for="name">Name:</label>
+   <input id="name" v-model="name" placeholder="name">
+ </p>
+
+ <p>
+   <label for="review">Review:</label>
+   <textarea id="review" v-model="review"></textarea>
+ </p>
+
+ <p>
+   <label for="rating">Rating:</label>
+   <select id="rating" v-model.number="rating">
+     <option>5</option>
+     <option>4</option>
+     <option>3</option>
+     <option>2</option>
+     <option>1</option>
+   </select>
+ </p>
+ <label for="recom">Would you recommend this product?</label>
+ <p>Yes<input name="answer" type="radio" value="yes" id="recom" v-model="answer"> </p>
+ <p>No<input name="answer" type="radio" value="no" id="recom" v-model="answer"></p>
+ <p>
+   <input type="submit" value="Submit"> 
+ </p>
+
+
+
+</form>
+
+  `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            answer: null,
+            errors: []
+
+        }
+    },
+    methods:{
+        onSubmit() {
+            if(this.name && this.review && this.rating && this.answer) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    answer: this.answer
+
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+                this.answer = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+                if(!this.answer) this.errors.push("Give answer.")
+            }
+         }
+         
+     }
+     
+ })
+ 
  let app = new Vue({
     el: '#app',
     data: {
         premium: true,
-        cart: []
+        cart: [],
+        reviews: []
     },
     methods: {
         updateCart(id) {
@@ -104,8 +202,9 @@ Vue.component('product', {
         },
         deleteCart(id){
             this.cart.pop(id);
-        }
-     }
+        },
+     },
+     
      
  })
  
